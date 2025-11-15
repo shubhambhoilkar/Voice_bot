91520581aabfec7c549a2038ae4d3b7452836905
Shubham Bhoilkar <91750972+shubhambhoilkar@users.noreply.github.com>
2025-11-15 15:10:46 +0530

    Modify API URL and clean up code formatting
    Updated API URL and made minor adjustments to comments and formatting.

--- Sam_voice_bot.py
+++ Sam_voice_bot.py
@@ -17,7 +17,7 @@ CHANNELS = 1
 BLOCK_SIZE = 512
 VAD_SILENCE_TIMEOUT = 2.5
 MIN_SEGMENT_SEC = 0.5
-API_URL = "https://Sam.chatbot/"
+API_URL = "localhost:/Sam_voicebot"
 
 # Smart endpointing / merging tuning
 SPEECH_END_PADDING = 0.4
@@ -450,4 +450,5 @@ except KeyboardInterrupt:
     print("Exiting...")
     stream.stop()
     stream.close()
+
 
